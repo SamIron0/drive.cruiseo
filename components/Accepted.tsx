44f34a5ecf086bebca26c5feaba4a94a9ad87788
SamIron0 <samuelironkwec@gmail.com>
2024-06-03 18:58:04 -0500

    x

--- components/Accepted.tsx
+++ components/Accepted.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Container from "./Container"
-import ListingCard from "./listings/ListingCard"
 import ClientOnly from "./ClientOnly"
 import { useContext, useEffect } from "react"
 import { CruiseoContext } from "@/context/context"
@@ -36,81 +35,6 @@ function editDistance(a: string, b: string): number {
 }
 interface GridProps {}
 export function Grid() {
-  const {
-    destinations,
-    setDestinations,
-    setProfile,
-    searchInput,
-    activeCategory
-  } = useContext(CruiseoContext)
-
-  useEffect(() => {
-    const fetchDestinations = async () => {
-      try {
-        const userID = await supabase.auth.getUser()
-        const url = "/api/getDestinations"
-        const options = {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-
-        const response = await fetch(url, options)
-        setDestinations(await response.json())
-        const profile = userID.data.user
-          ? await getProfileByUserId(userID.data.user?.id as string)
-          : null
-        setProfile(profile)
-      } catch (err) {
-        console.error(err)
-      }
-    }
-
-    fetchDestinations()
-  }, [])
-
-  const filteredListings =
-    activeCategory === "All"
-      ? destinations?.filter(listing =>
-          listing.name.toLowerCase().includes(searchInput.toLowerCase())
-        )
-      : destinations?.filter(
-          listing =>
-            listing.category === activeCategory &&
-            listing.name.toLowerCase().includes(searchInput.toLowerCase())
-        )
-
-  const sortedListings = filteredListings?.sort((a, b) => {
-    const aScore = editDistance(a.name.toLowerCase(), searchInput.toLowerCase())
-    const bScore = editDistance(b.name.toLowerCase(), searchInput.toLowerCase())
-    return aScore - bScore
-  })
-
-  return (
-    <ClientOnly>
-      <Container>
-        {destinations ? (
-          <div
-            className="
-              py-[198px]
-              grid 
-              grid-cols-1 
-              sm:grid-cols-2 
-              md:grid-cols-3 
-              lg:grid-cols-4
-              xl:grid-cols-5
-              2xl:grid-cols-6
-              gap-7
-            "
-          >
-            {sortedListings?.map(listing => (
-              <ListingCard key={listing.id} data={listing} />
-            ))}
-          </div>
-        ) : (
-<></>        )}
-      </Container>
-    </ClientOnly>
-  )
+  
+  return (<></>)
 }
