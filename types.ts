abcb6935c39bcb980791d0d15f50b1bd75233d58
SamIron0 <samuelironkwec@gmail.com>
2024-06-03 19:07:37 -0500

    d

---
+++ types.ts
@@ -0,0 +1,44 @@
+import { Json, Tables } from "./supabase/types"
+
+export interface Date {
+  date: string
+  hour: string
+  minute: string
+  ampm: string
+}
+export interface Trip {
+  id: string
+  user_id?: string
+  origin?: string
+  destination?: string
+  price?: number
+  pickup?: Date
+  status?: string
+  dropoff?: Date
+}
+
+export interface GeoCoordinate {
+  lat: number
+  lon: number
+}
+export interface Destination {
+  id: string
+  address: string
+  category: string
+  photo: string
+  trip_ids?: string[]
+  name: string
+  times?: string[] | null
+  activeTrips?: Tables<"trips">[] | null
+  coordinates?: GeoCoordinate | null
+}
+export interface UserDetails {
+  trips: Tables<"trips">[] | null
+  avatar_url: string | null
+  billing_address: Json | null
+  full_name: string | null
+  id: string
+  address: string | null
+  payment_method: Json | null
+  geolocation?: GeoCoordinate | null
+}
